164d2ca90c56ad0d3abe1298c20c7549c2dcd675
Mahesh Gilbile <mgilbile819@gmail.com>
2024-07-08 23:42:34 +0530

    Update prometheusPush.groovy

--- vars/prometheusPush.groovy
+++ vars/prometheusPush.groovy
@@ -2,7 +2,7 @@
 
 import io.prometheus.client.exporter.PushGateway
 import io.prometheus.client.CollectorRegistry
-import io.prometheus.client.Counter
+import io.prometheus.client.Gauge
 
 def call(Map params) {
     def gateway = params.gateway
@@ -14,42 +14,42 @@ def call(Map params) {
     def registry = new CollectorRegistry()
 
     // Define and register Prometheus metrics
-    def totalBuilds = Counter.build()
+    def totalBuilds = Gauge.build()
         .name("total_builds")
         .help("Total number of builds")
         .labelNames("app_name")
         .register(registry)
 
-    def successBuilds = Counter.build()
+    def successBuilds = Gauge.build()
         .name("success_builds")
         .help("Total number of successful builds")
         .labelNames("app_name")
         .register(registry)
 
-    def failedBuilds = Counter.build()
+    def failedBuilds = Gauge.build()
         .name("failed_builds")
         .help("Total number of failed builds")
         .labelNames("app_name")
         .register(registry)
 
-    def buildTime = Counter.build()
+    def buildTime = Gauge.build()
         .name("build_time_seconds")
         .help("Average build time in seconds")
         .labelNames("app_name")
         .register(registry)
 
-    def successRate = Counter.build()
+    def successRate = Gauge.build()
         .name("success_rate")
         .help("Success rate of builds")
         .labelNames("app_name")
         .register(registry)
 
     // Set metrics values
-    totalBuilds.labels(appName).inc(metrics.total_builds ?: 0)
-    successBuilds.labels(appName).inc(metrics.total_success_builds ?: 0)
-    failedBuilds.labels(appName).inc(metrics.total_failed_builds ?: 0)
-    buildTime.labels(appName).inc(metrics.average_build_time ?: 0)
-    successRate.labels(appName).inc(metrics.success_rate ?: 0)
+    totalBuilds.labels(appName).set(metrics.total_builds ?: 0)
+    successBuilds.labels(appName).set(metrics.total_success_builds ?: 0)
+    failedBuilds.labels(appName).set(metrics.total_failed_builds ?: 0)
+    buildTime.labels(appName).set(metrics.average_build_time ?: 0)
+    successRate.labels(appName).set(metrics.success_rate ?: 0)
 
     // Push metrics to Pushgateway
     try {
