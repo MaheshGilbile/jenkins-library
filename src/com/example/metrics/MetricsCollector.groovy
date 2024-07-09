5bc9e24743b0b723aea8bf2280ac2ad7ce8576a4
Mahesh Gilbile <mgilbile819@gmail.com>
2024-07-09 17:28:37 +0530

    Update MetricsCollector.groovy

--- src/com/example/metrics/MetricsCollector.groovy
+++ src/com/example/metrics/MetricsCollector.groovy
@@ -1,19 +1,20 @@
 package com.example.metrics
 
-import jenkins.model.Jenkins
 import groovy.sql.Sql
+import java.text.SimpleDateFormat
 import java.util.Date
 
 @Grab(group='org.postgresql', module='postgresql', version='42.7.2')
 
 class MetricsCollector {
-
-    def recordMetrics(stageName, status, env) {
+    void recordMetrics(String stageName, String status, Map env) {
         def project = Jenkins.instance.getItemByFullName(env.JOB_NAME)
-        def totalBuilds = project.getBuilds().size()
-        def totalSuccessBuilds = project.getBuilds().findAll { it.result.toString() == 'SUCCESS' }.size()
+        def builds = project.getBuilds()
+
+        def totalBuilds = builds.size()
+        def totalSuccessBuilds = builds.findAll { it.result.toString() == 'SUCCESS' }.size()
         def totalFailedBuilds = totalBuilds - totalSuccessBuilds
-        def totalSuccessRate = totalBuilds > 0 ? (totalSuccessBuilds / totalBuilds) * 100.0 : 0.0
+        def totalSuccessRate = totalBuilds > 0 ? (totalSuccessBuilds * 100.0 / totalBuilds) : 0.0
 
         def metrics = [
             'stage_name': stageName,
@@ -26,17 +27,22 @@ class MetricsCollector {
             'total_failed_builds': totalFailedBuilds,
             'total_success_rate': totalSuccessRate
         ]
-
+        
+        // Insert metrics into PostgreSQL database
         insertMetricsIntoDatabase(metrics, env)
     }
 
-    def insertMetricsIntoDatabase(metrics, env) {
-        def sql = Sql.newInstance(env.DB_URL, env.DB_USER, env.DB_PASS, 'org.postgresql.Driver')
+    void insertMetricsIntoDatabase(Map metrics, Map env) {
+        def dbUrl = env.DB_URL
+        def dbUser = env.DB_USER
+        def dbPass = env.DB_PASS
+
+        def sql = Sql.newInstance(dbUrl, dbUser, dbPass, 'org.postgresql.Driver')
 
         try {
             sql.execute("""
                 INSERT INTO AppFitMetrics (application_name, application_shortname, application_id, branch_name, scm_status, unit_test_status, sonar_status, artifactory_upload, total_success_builds, total_failed_builds, total_success_rate, created_at)
-                VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+                VALUES (?, ?, ?, ?, ?, '', '', '', ?, ?, ?, ?)
                 """,
                 [
                     metrics['app_name'],
@@ -44,9 +50,6 @@ class MetricsCollector {
                     metrics['app_id'],
                     metrics['branch_name'],
                     metrics['stage_name'] == 'SCM Checkout' ? metrics['status'] : '',
-                    metrics['stage_name'] == 'Unit Test Coverage' ? metrics['status'] : '',
-                    metrics['stage_name'] == 'Sonar Scanning' ? metrics['status'] : '',
-                    metrics['stage_name'] == 'Artifactory Upload' ? metrics['status'] : '',
                     metrics['total_success_builds'],
                     metrics['total_failed_builds'],
                     metrics['total_success_rate'],
